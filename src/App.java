57e9e7a76f084536cd38971c1e6d3f40ad118177
Guillaume Aime A Bogaert <guillaumebogaert244@gmail.com>
2026-08-26 20:47:33 +0200

    added exercise file

--- src/App.java
+++ src/App.java
@@ -1,7 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
        Athlete athlete = new Athlete ("Guillaume");
+       Exercise exercise = new Exercise ("Bench press", 3, 6, "3-1-1-0");
 
        System.out.println(athlete);
+       System.out.println(exercise);
     }
 }
