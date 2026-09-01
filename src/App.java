a4d9897be8423b25608ed03f57f3d97270488a6d
Your Name <your@email.com>
2026-09-01 18:35:22 +0200

    pull up

--- src/App.java
+++ src/App.java
@@ -8,6 +8,8 @@ public class App {
 
         Exercise exercise = new Exercise("Bench press", 3 , 5, "4-1-3-2");
         workout.addExercise(exercise);
+        Exercise exercise2 = new Exercise("Pull up", 3, 8, "4-1-0-1");
+        workout.addExercise(exercise2);
 
         // add it
         library.addWorkout(workout);
