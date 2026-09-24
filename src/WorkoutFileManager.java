a16b7425ef5dddecc2e0e1a5556e63f2acfc9b6b
Guillaume Aime A Bogaert <guillaumebogaert244@gmail.com>
2026-09-24 10:40:52 -0400

    work on files

--- src/WorkoutFileManager.java
+++ src/WorkoutFileManager.java
@@ -1,5 +1,7 @@
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class WorkoutFileManager {
 
@@ -7,9 +9,51 @@ public class WorkoutFileManager {
         FileWriter filewriter = new FileWriter("workouts.txt");
 
         for (Workout workout : library.getWorkouts())  {
-            filewriter.write(workout.toString() + "\n");
+            filewriter.write("WORKOUT: " + workout.getName() + "\n");
+
+            for (Exercise exercise : workout.getExercises()) {
+                filewriter.write("EXERCISE: " + exercise.getName() + ", " + exercise.getSets()+ ", "+ exercise.getReps()+ ", " + exercise.getTempo() + "\n");
+            }
         }
         filewriter.close();
+    }
+
+    public void loadWorkouts(WorkoutLibrary library) throws IOException {
+        File file = new File("workouts.txt");
+        if (!file.exists()) {
+            return; //no files to load. 
+        }
+
+        Scanner fileScanner = new Scanner(file);
+
+        Workout currentWorkout = null;
+
+        while (fileScanner.hasNextLine()) {
+            String line = fileScanner.nextLine();
+
+            if (line.startsWith("WORKOUT: ")) {
+                String workoutName = line.substring(9);
+
+                currentWorkout = new Workout(workoutName);
+                library.addWorkout(currentWorkout);
+            } 
+            else if (line.startsWith("EXERCISE: ")) {
+                String exerciseData = line.substring(10);
+
+                String[] parts = exerciseData.split(", ");
+                String name = parts[0];
+                int sets = Integer.valueOf(parts[1]);
+                int reps = Integer.valueOf(parts[2]);
+                String tempo = parts[3];
+
+                Exercise exercise = new Exercise (name, sets, reps, tempo);
+
+            }
+
+            
+
+        }
+
 
     }
 }
